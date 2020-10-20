127b81294cb75df3ff3c50e2f8dc7a80570f2c0f
huburtx <73172489+huburtx@users.noreply.github.com>
2020-10-20 18:01:59 +0200

    Add files via upload

---
+++ ComboJack_Installer/uninstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]];
+then
+    exec sudo /bin/bash "$0" "$@"
+fi
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# uninstall
+sudo spctl --master-disable
+sudo mount -uw /
+sudo killall Finder
+sudo launchctl unload /Library/LaunchDaemons/com.XPS.ComboJack.plist
+sudo rm /Library/LaunchDaemons/com.XPS.ComboJack.plist
+sudo rm /usr/local/share/ComboJack/l10n.json
+sudo rm /usr/local/share/ComboJack/Headphone.icns
+sudo rm -r /usr/local/share/ComboJack
+sudo rm /usr/local/sbin/hda-verb
+sudo spctl --remove /usr/local/sbin/ComboJack
+sudo rm /usr/local/sbin/ComboJack
+echo
+echo "Please reboot! Remember to remove VerbStub.kext from EFI/OC/Kexts."
+echo
+exit 0
